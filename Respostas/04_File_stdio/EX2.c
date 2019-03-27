c7b90ddd800ce84c1fbaaa8cee864b2883c84330
Gustavo Caltabiano Eichler <30873998+gustavoeichler@users.noreply.github.com>
2019-03-27 08:51:25 -0300

    Update EX2.c

--- Respostas/04_File_stdio/EX2.c
+++ Respostas/04_File_stdio/EX2.c
@@ -1,32 +1,32 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(int argc, const char * argv[]) {
-   
-  
-  
-    /* Le um nome para o arquivo a ser aberto: */
-    char name[30];
-    int a=0;
-    printf("Digite o seu nome:\n");
-    gets(name);
-    printf("Digite a sua idade:\n");
-    scanf("%d",&a);
- FILE *fp;
-    fp=fopen("ola_usuario_1.txt","w"); /* Arquivo ASCII, para leitura */
-   if(!fp)
-   {
-       printf( "Erro na abertura do arquivo");
-       exit(-1);
-   }
-    /* Se nao houve erro, imprime no arquivo e o fecha ...*/
-   
-        fputs("Nome:",fp);
-        fputs(name,fp);
-        putc('\n', fp);
-        fputs("Idade:",fp);
-        fprintf(fp, "%d", a); 
-        
-    fclose(fp);
-    }
-    
+
+int main(int argc , char *argv[])
+{
+FILE *fp;
+char frase[200] = "Nome: ";
+char idade_frase[200] = "Idade: ";
+int i=0;
+fp = fopen("Eu.txt", "w");
+
+if(!fp)
+{
+  //Arquivo ASCII, para escrita
+  printf("Erro na leitura do arquivo");
+  exit(0);
+}
+
+
+strcat(frase,argv[1]);
+
+strcat(idade_frase, argv[2]);
+for(i=0; frase[i]; i++)
+  putc(frase[i],fp);
+putc('\n',fp);
+for(i=0; idade_frase[i]; i++)
+  putc( idade_frase[i],fp);
+fclose(fp);
+return 0;
+}
